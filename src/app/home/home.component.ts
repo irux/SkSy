684c3e2acf3156993fcabbfba0b28b493df0e3d4
Sara Kadenaj <sara.kadena@gmail.com>
2019-05-13 21:01:06 +0200

    Addding Datepicker

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -42,6 +42,7 @@ export class HomeComponent implements OnInit {
 
 
   public async createTodo(todo){
+    
     if(this.newTodo.title == '' || this.newTodo.deadline == '' || this.newTodo.description == '')
       return;
     console.log("Here is the todo : ")
@@ -57,6 +58,7 @@ export class HomeComponent implements OnInit {
 
   public changeDateInPicker(dateObject){
     this.newTodo.deadline = `${dateObject.day}/${dateObject.month}/${dateObject.year}`
+    console.log(dateObject)
   }
   
 
